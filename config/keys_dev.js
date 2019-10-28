e0accfe46b4795a092d598d2fe15e32b7d5116b3
AbonyiXavier <francisabonyi@gmail.com>
2019-10-27 22:53:01 -0700

    initial commit

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stripePublishableKey: "pk_test_JULxsbzmbMr0PPek9VdqqULO00vQMKJP8e",
+  stripeSecretKey: "sk_test_LGz4zkligoF4tmT9eqUFTQkg00ZLvWyKIM"
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const keys = require("./config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+// Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Static Folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index Route
+app.get("/", (req, res) => {
+  res.render("index", {
+    stripePublishableKey: stripePublishableKey
+  });
+});
+
+// Charge Route
+app.post("/pay", (req, res) => {
+  const amount = 3500;
+
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      // source: req.body.stripeToken
+      source: "tok_visa"
+    })
+    .then(customer =>
+      stripe.charges.create({
+        amount,
+        description: "BuyMyArt",
+        currency: "usd",
+        customer: customer.id
+      })
+    )
+    .then(charge => res.render("pay"));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
